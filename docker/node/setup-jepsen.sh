824f7b227b0b30157d5eb1f59e67cabc3785c0aa
Ege Berkay Gulcan <egeberkaygulcan@gmail.com>
2025-06-15 12:45:43 +0200

    Update setup-jepsen.sh

--- docker/node/setup-jepsen.sh
+++ docker/node/setup-jepsen.sh
@@ -5,12 +5,12 @@ echo "Adding hostname to shared volume" >> /var/log/jepsen-setup.log
 hostname >> /var/jepsen/shared/nodes
 
 # Copy instrumentation IDs to shared volume
-cp /opt/instrumentor/*_CodeLocToBranchID.txt /var/jepsen/shared/
-cp /opt/instrumentor/*_FuncNameToID.txt /var/jepsen/shared/
+cp "/opt/instrumentor/redisraft_CodeLocToBranchID.txt" /var/jepsen/shared/
+cp "/opt/instrumentor/*_FuncNameToID.txt" /var/jepsen/shared/
 
 # We make sure that root's authorized keys are ready
 echo "Setting up root's authorized_keys" >> /var/log/jepsen-setup.log
-mkdir /root/.ssh
+mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 cp /run/secrets/authorized_keys /root/.ssh/
 chmod 600 /root/.ssh/authorized_keys
